467aaefd3db71b1ee89309088907397e0941c880
Beliy Nikita <nikita.beliy@uliege.be>
2018-12-04 12:45:30 +0100

    Channel now owns and treat the stream
    Stream is readed during __init__, the measure points are readed
    directly from file as needed

--- DataStructure/Channel.py
+++ DataStructure/Channel.py
@@ -1,9 +1,11 @@
 import struct
-from datetime import datetime
+import io
+from datetime import datetime, timedelta
 
 #Values: ['Field name', 'data size in words, 0 if unknown, not fixed', 'parcing word, c if it is text',  'encoding string']
 
 class Field(object):
+    """ Class describes type of data and how to read it"""
     __slots__ = ["Name", "Size", "IsText", "Format", "Encoding", "Entries"]
     def __init__(self, Name, Format, Size = 0, IsText = False, Encoding = "ascii", Entries = 0, Unique = False):
         self.Name     = Name
@@ -29,6 +31,7 @@ class Field(object):
     def IsUnique(self):
         return (self.Entries == 1)
 
+""" A dictionary of fields in the ebm file, each entry will create a corresponding field in channel class  """
 Marks = {
     b'\x80\x00\x00\x00' : Field("Version", "B", Size = 2, Unique = True),
     b'\x81\x00\x00\x00' : Field("Header", "x", IsText = True, Unique = True),
@@ -73,20 +76,75 @@ Marks = {
     b'\x72\x00\x00\x03' : Field("SigMainType", "h", IsText = True, Unique = True),
     b'\x74\x00\x00\x03' : Field("SigSubType", "h", IsText = True, Unique = True),
    } 
-#b'\x1a\x00\x00\x00' : ["SigEnd",        0, ""],
-#b'\xff\x00\x00\x00' : ["Invalid",       0, "b"],
 
 class Channel(object):
-    __slots__ = [x.Name for x in list(Marks.values())]+["Endian", "Wide"]
-    def __init__(self, end, wide):
+    """ Class containing all information retrieved from ebm file. The data instead to be loaded in the memory, are readed directly from file """
+    __slots__ = [x.Name for x in list(Marks.values())]+["Endian", "Wide", "_stream", "_seqStart", "_seqSize", "_totSize", "_dataSize"]
+    def __init__(self, stream):
         for f in self.__slots__:
             setattr(self, f, [])
-        self.Endian = end
-        self.Wide = wide
-        if wide:
+        if not isinstance(stream, (io.RawIOBase, io.BufferedIOBase)):
+            raise Exception("Stream is not valid")
+
+        self._stream = stream
+        self._stream.seek(0)
+        #Reading header
+        buff = b''
+        ch = self._stream.read(1)
+        
+        while ch != b'\x1a':
+            buff = buff+ch
+            ch = self._stream.read(1)
+
+        if (buff.decode('ascii') == 'Embla data file'):
+            print("We reading Embla data file")
+        elif (buff.decode('ascii') == 'Embla results file'):
+            print("We are reading Embla results file")
+        elif (buff.decode('ascii') == 'Embla raw file'):
+            print("We are reading Embla raw file")
+        else:
+            print("We are not reading either Embla results or Embla data")
+            raise Exception("We are not reading either Embla results or Embla data")
+        ch = self._stream.read(1)
+        if ch == b'\xff':
+            self.Endian = '>'
+            print("We using big-endian")
+        elif ch == b'\x00':
+            self.Endian = '<'
+            print("We using little-endian")
+        else:
+            raise Exception("Can't determine endian")
+
+        self.Wide = False
+        ch = self._stream.read(1)
+        if (ch == b'\xff'):
+            ch = self._stream.read(4)
+            if ch == b'\xff\xff\xff\xff':   
+                self.Wide = True
+                self._stream.seek(32 - 6,1)
+         
+        if self.Wide:
             Marks[b'\x20\x00\x00\x00'].Format = 'h'
+            self._dataSize = 2
         else:
             Marks[b'\x20\x00\x00\x00'].Format = 'b'
+            self._dataSize = 1
+        
+        while True:
+            start = self._stream.tell()
+            if self.Wide :
+                index = self._stream.read(4)
+            else:
+                index = self._stream.read(2)
+                index = index+b'\x00\x00'
+            if(index == b''):break
+            size = self._stream.read(4)
+            size = struct.unpack("<L", size)[0]
+            #data = self._stream.read(size)
+            self.__read(index, size)
+        self._totSize = sum(self._seqSize)
+
+
     
     def __str__(self):
         string = ""
@@ -105,32 +163,38 @@ class Channel(object):
                     string = string + f + '\t' + str(getattr(self, f))+ '\n'
         return string
 
-    def read(self, marker, data):
+    def __read(self, marker, size):
         dtype = Marks[marker]
         try:
-            fname = dtype.Name
-            fsize = dtype.Size
-            ftype = dtype.Format
-            fenc  = dtype.Encoding
+            fname = dtype.Name 
+            fsize = dtype.Size 
+            ftype = dtype.Format 
+            fenc  = dtype.Encoding 
             #tsize represents the a size of the entry, for text it is fixed to 1
             if dtype.IsText:
                 tsize = 1
             else:
                 tsize = int(struct.calcsize(self.Endian+ftype))
-            dsize = len(data)
-            nwords = int(dsize/tsize)
-
+            dsize = size #Lenth of the field
+            nwords = int(dsize/tsize) #Number of entries in the field
+ 
             if fsize > 0 and nwords != fsize:
                 raise Exception("Field contains {} words, {} requested".format(nwords, fsize))
 
             if dtype.IsText:
+                text = self._stream.read(size).decode(fenc).strip('\0')
                 if dtype.IsUnique():
-                    setattr(self, fname, data.decode(fenc).strip('\0'))
+                    setattr(self, fname, text)
                 else:
-                    setattr(self, fname, getattr(self, fname)+[data.decode(fenc).strip('\0')])
+                    setattr(self, fname, getattr(self, fname)+[text])
             else:
+                if fname == "Data":
+                    self._seqStart.append(self._stream.tell())
+                    self._seqSize.append(nwords)
+                    self._stream.seek(size, 1)
+                    return
                 dec = self.Endian + ftype*nwords + 'x'*(dsize - tsize*nwords)
-                unpacked = struct.unpack(dec, data)
+                unpacked = struct.unpack(dec, self._stream.read(size))
                 if fname == "Version":
                     if self.Endian == '>':
                         big, small = unpacked
@@ -143,9 +207,6 @@ class Channel(object):
                     year, mon, day, h, m, s, us = unpacked
                     time = datetime(year, mon, day, h, m, s, us*10000)
                     self.Time = self.Time+[time]
-                elif fname == "Data":
-                    print(data[0:8])
-                    setattr(self, fname, getattr(self, fname)+[list(unpacked)])
                 else:
                     if dtype.IsUnique():
                         if len(unpacked) == 1:
@@ -154,77 +215,39 @@ class Channel(object):
                             setattr(self, fname, list(unpacked))
                     else:
                         setattr(self, fname, getattr(self, fname)+[list(unpacked)])
-
         except Exception as e:
             raise Exception("Unamble to parce {}:{}\n{}".format(dtype[0], data, e))
 
-
-
-def readChannel(stream):
-    #Reading header
-    buff = b''
-    ch = stream.read(1)
-
-
-    while ch != b'\x1a':
-        buff = buff+ch
-        ch = stream.read(1)
-
-    if (buff.decode('ascii') == 'Embla data file'):
-        print("We reading Embla data file")
-    elif (buff.decode('ascii') == 'Embla results file'):
-        print("We are reading Embla results file")
-    else:
-        raise Exception("We are not reading either Embla results or Embla data")
-
-    ch = stream.read(1)
-
-    if ch == b'\xff':
-        BigEndian = True
-        byteorder = 'big'
-        byteprefix= '>'
-        print("We using big-endian")
-    elif ch == b'\x00':
-        BigEndian = False
-        byteorder = 'little'
-        byteprefix= '<'
-        print("We using little-endian")
-    else:
-        raise Exception("Can't determine endian, format problrm?")
-
-    wideId = False
-    ch = stream.read(1)
-    if (ch == b'\xff'):
-        ch = stream.read(4)
-        if ch == b'\xff\xff\xff\xff':
-            wideId = True
-            print("We using wide Id")
-            stream.seek(32 - 6,1)
-    else:
-        print("We using normal Id")
-
-    if wideId :
-        base_size = 4
-    else:
-        base_size = 1
-    ch = Channel(byteprefix, wideId)
-
-    while True:
-        start = stream.tell()
-        if wideId :
-            index = stream.read(4)
+    def getSize(self, sequence = None):
+    """ Returns total size (nmb. of measure points) of dataset """
+        if sequence == None:
+            return self._totSize
         else:
-            index = stream.read(2)
-            index = index+b'\x00\x00'
+            return self._seqSize[sequence]
 
-        if(index == b''):break
-        #print(hex(start), index)
-        size = stream.read(4)
-        #print(size)
-        size = struct.unpack("<L", size)[0]
-        #print("Size:",size)
-        data = stream.read(size)
-        ch.read(index, data)    
+    def getValue(self, point, sequence = None):
+    """ Returns value of given measure point. If sequance is given, the point should be in correspondent sequance. If no suequance is given, the point is interpreted as global one """
+        if sequence == None:
+            point, sequence = self.getRelPoint(point)
 
-    return ch
+        self._stream.seek(self._seqStart[sequence] + (point)*self._dataSize)
+        return struct.unpack(self.Endian+Marks[b'\x20\x00\x00\x00'].Format, self._stream.read(self._dataSize))[0]*self.Gain/1000
 
+    def getRelPoint(self, point):
+    """ Returns a tuple (point, sequance) for absolute point index """
+        for sequence in range(0, len(self._seqSize)):
+            if point >= self._seqSize[sequence]:
+                point = point - self._seqSize[sequence]    
+            else : break
+        return (point, sequence)
+
+    def getTime(self, point, sequence = None):
+    """ If sequance given, returns nmb. of seconds passed cince the start of sequence, or secons scince the start of recording (first sequence) """
+        if sequence == None:
+            point, seq = self.getRelPoint(point)
+            t = self.Time[seq] + timedelta(seconds = point/self.DBLsampling)
+            return (t - self.Time[0]).total_seconds()
+        else : 
+            return(point/self.DBLsampling)
+
+        
--- data_parcer.py
+++ data_parcer.py
@@ -1,85 +1,37 @@
 import struct
 from datetime import datetime
 
-from Channel import Channel
-from Channel import Marks
+from DataStructure.Channel import Channel
+from DataStructure.Channel import Marks
 
 #f = open("testdata.ebm", "rb")
 #f = open("../data_test/EEG/BL/c6902c41-642b-4dd2-88b2-81e03feba944/C3.ebm", "rb")
 f = open("data_example/embla_data/C3.ebm", "rb")
 
-#Reading header
-buff = b''
-ch = f.read(1)
-
-
-while ch != b'\x1a':
-    buff = buff+ch
-    ch = f.read(1)
-
-if (buff.decode('ascii') == 'Embla data file'):
-    print("We reading Embla data file")
-elif (buff.decode('ascii') == 'Embla results file'):
-    print("We are reading Embla results file")
-else:
-    print("We are not reading either Embla results or Embla data")
-    exit()
-
-ch = f.read(1)
-
-if ch == b'\xff':
-    BigEndian = True
-    byteorder = 'big'
-    byteprefix= '>'
-    print("We using big-endian")
-elif ch == b'\x00':
-    BigEndian = False
-    byteorder = 'little'
-    byteprefix= '<'
-    print("We using little-endian")
-else:
-    print("Can't determine endian, format problrm?")
-    exit()
-
-wideId = False
-ch = f.read(1)
-if (ch == b'\xff'):
-    ch = f.read(4)
-    if ch == b'\xff\xff\xff\xff':   
-        wideId = True
-        print("We using wide Id")
-        f.seek(32 - 6,1)
-else:
-    print("We using normal Id")
-    
-if wideId :
-    base_size = 4
-else:
-    base_size = 1
-ch = Channel(byteprefix, wideId)
-
-while True:
-    #print("---------------------")
-    start = f.tell()
-    if wideId :
-        index = f.read(4)
-    else:
-        index = f.read(2)
-        index = index+b'\x00\x00'
-        
-    if(index == b''):break
-    #print(hex(start), index)
-    size = f.read(4)
-    #print(size)
-    size = struct.unpack("<L", size)[0]
-    #print("Size:",size)
-    data = f.read(size)
-    ch.read(index, data)
+ch = Channel(f)
 
 print(str(ch))
 print("RawRange:", ch.RawRange[0:10])
 print("TransRange:", ch.TransRange[0:10])
-print("Data:", ch.Data[0][0:10])
 print("SigType:", ch.SigType[0:10])
 print("SigRef:", ch.SigRef[0:10])
 print("LowHight:", ch.LowHight[0:10])
+
+print(ch.getTime(0,0), ch.getValue(0,0))
+print(ch.getTime(1,0), ch.getValue(1,0))
+print(ch.getTime(2,0), ch.getValue(2,0))
+print(ch.getTime(3,0), ch.getValue(3,0))
+print(ch.getTime(4,0), ch.getValue(4,0))
+print("####################")
+print(ch.getTime(0,1), ch.getValue(0,1))
+print(ch.getTime(1,1), ch.getValue(1,1))
+print(ch.getTime(2,1), ch.getValue(2,1))
+print(ch.getTime(3,1), ch.getValue(3,1))
+print(ch.getTime(4,1), ch.getValue(4,1))
+print("####################")
+print(ch.getRelPoint(1234))
+print(ch.getTime(1234), ch.getValue(1234))
+print("####################")
+print(ch.getRelPoint(99))
+print(ch.getTime(99), ch.getValue(99))
+
