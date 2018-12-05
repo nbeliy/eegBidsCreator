4e04e2036505792baf57447337aaaa0740e8a60b
Beliy Nikita <nikita.beliy@uliege.be>
2018-12-05 12:37:44 +0100

    Parce xml firelds from Rceording file

---
+++ DataStructure/Record.py
@@ -0,0 +1,38 @@
+import xml.etree.ElementTree as ElementTree
+from datetime import datetime
+
+def addDict(parent):
+    d = dict()
+    for child in parent:
+        data = child.get('{urn:schemas-microsoft-com:datatypes}dt', None)
+        if data != None :
+            del child.attrib['{urn:schemas-microsoft-com:datatypes}dt']
+            try:
+                if data == 'string':
+                    d[child.tag] = child.text
+                elif data == 'datetime':
+                    d[child.tag] = datetime.strptime(child.text+"000", "%Y-%m-%dT%H:%M:%S.%f")
+                elif data == 'r8':
+                    d[child.tag] = float(child.text)
+                elif data == 'i2':
+                    d[child.tag] = int(child.text)
+                else :
+                    raise Exception("Unown type {}".format(data))
+            except Exception as e:
+                print(e)
+                d[child.tag] = child.text
+            d.update(parent.attrib)
+        else : 
+            d[child.tag] = addDict(child)
+    d.update(parent.attrib)
+    return d
+    
+
+
+
+def ParceRecording(xml):
+    root = ElementTree.fromstring(xml)
+    return addDict(root)
+    
+    
+
