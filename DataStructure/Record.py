35972846e2175ee4b8bc462b23b7c267e0d464c6
Beliy Nikita <nikita.beliy@uliege.be>
2018-12-05 14:40:23 +0100

    Added i4 type and fixed empty text for Record

--- DataStructure/Record.py
+++ DataStructure/Record.py
@@ -8,16 +8,19 @@ def addDict(parent):
         if data != None :
             del child.attrib['{urn:schemas-microsoft-com:datatypes}dt']
             try:
-                if data == 'string':
-                    d[child.tag] = child.text
-                elif data == 'datetime':
-                    d[child.tag] = datetime.strptime(child.text+"000", "%Y-%m-%dT%H:%M:%S.%f")
-                elif data == 'r8':
-                    d[child.tag] = float(child.text)
-                elif data == 'i2':
-                    d[child.tag] = int(child.text)
-                else :
-                    raise Exception("Unown type {}".format(data))
+                if child.text = None:
+                    d[child.tag] = None
+                else:
+                    if data == 'string':
+                        d[child.tag] = child.text
+                    elif data == 'datetime':
+                        d[child.tag] = datetime.strptime(child.text+"000", "%Y-%m-%dT%H:%M:%S.%f")
+                    elif data == 'r8':
+                        d[child.tag] = float(child.text)
+                    elif data == 'i2' or data == 'i4':
+                        d[child.tag] = int(child.text)
+                    else :
+                        raise Exception("Unown type {}".format(data))
             except Exception as e:
                 print(e)
                 d[child.tag] = child.text
