529a58245c6ac5119d8c27fc882720afb9aea427
Beliy Nikita <beliy.nikita@outlook.com>
2019-01-16 10:09:40 +0100

    Example bash script
    Script will run eegBidsCreator over all folders in given directory in
    semi-parralel way

---
+++ Example/run_test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+n_parralel=4 
+count=0
+
+def_path='/media/beliy/KINGSTON/Memodyn/ForTest/Emb'
+ini_file="Example/COF_BH.ini"
+out_path="output/ForTest"
+
+run(){
+  base=`basename $1`
+  base=${base:0:8}
+  python3 eegBidsCreator.py -c $ini_file -a $base -o $out_path -q --logfile $out_path/$base.log $file
+  retval="$base returned value: $?"
+}
+
+if [ $# -eq 0 ]; then
+  set -- $def_path
+fi  
+
+while [ $# -gt 0 ]; do
+  path="$1/*"
+  shift
+  echo "Going into $path"
+  for file in `ls -d $path`; do 
+    ((count++))
+    base=`basename $file`
+    base=${base:0:8}
+    (run $file; echo $retval)&
+    if [ $(($count%$n_parralel)) -eq 0 ]; then
+      wait
+    fi
+  done
+  wait
+
+done
