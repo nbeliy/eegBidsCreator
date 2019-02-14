d67f661f3aebefb0100576fd41802a1b4b2d0032
Beliy Nikita <beliy.nikita@uliege.be>
2019-02-14 13:37:49 +0100

    script returns 1xx if plugin fails

--- Example/Performance_Test.sh
+++ Example/Performance_Test.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
 n_tries=1000
-cmd="python3 eegBidsCreator.py -t HN -s 080518 -a T233900 -j Example/COF_HN.json -o output -c test/Test.ini  /media/beliy/KINGSTON/Memodyn/ForTest/Emb/35d1ef92-1b65-42f3-aab4-b3d61076f448/ --conversion "
-conv=(BV EDF)
+cmd="python3 eegBidsCreator.py -q -t HN -s 080518 -a T233900 -j Example/COF_HN.json -o /Storage/beliy/COFITAGE/Public/BIDS/ -c test/Test.ini  /Storage/beliy/COFITAGE/Public/Emb/35d1ef92-1b65-42f3-aab4-b3d61076f448/ --conversion "
+conv=(BV)
+
+#First run command to insure it works properly
+for c in ${conv[@]}; do
+  rm "Perf_"$c".log" | true
+  $cmd $c
+  if [ $? != "0" ]; then
+    echo "Command $cmd $c returned error"
+    exit 0
+  fi
+done
+
 
 for c in ${conv[@]}; do
   perf="Perf_"$c".log"
